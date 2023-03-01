01ae3815ce39f05962d7cbdfa20c29f83e861c7c
Egahi S. O <okworisamuel6@gmail.com>
2023-03-01 12:32:21 +0100

    Add project files.

---
+++ Parity/Parity.cpp
@@ -0,0 +1,44 @@
+/**
+ * Parity.cpp : This file contains the 'main' function. Program execution begins and ends there.
+ *
+ * 03/01/2023
+ */
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    // Greetings
+    cout << "Hello! This is parity!" << endl;
+
+    // Get a whole number from the user
+    num;
+    cout << "Enter a number: ";
+    cin >> num;
+
+    // Determine if the number is odd or even
+    if (num % 2 = 0)
+    {
+        cout << "The number is odd." << endl;
+    }
+    else
+    {
+        cout << "The number is even." << endl;
+    }
+
+    // end
+    return 0;
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
